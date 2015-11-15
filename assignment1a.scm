2d335c2aedab908fff9fc37bd0886afb4afcf0c6
kohd <shane.farrell@yahoo.ie>
2015-11-15 13:15:41 +0000

    CS424 assignment 1 - set-cardinality function.

---
+++ assignment1a.scm
@@ -0,0 +1,14 @@
+(define (member? element set)
+    (cond
+        [(empty? set) #f]
+        [(equal? element (car set)) #t]
+        [else (member? element (cdr set))]))
+
+(define (set-cardinality set)
+    (define (countElements set numElements)
+        (cond
+            [(empty? set) numElements]
+            [(member? (car set) (cdr set))
+                (countElements (cdr set) numElements)]
+            [else (countElements (cdr set) (+ numElements 1))]))
+    (countElements set 0))
