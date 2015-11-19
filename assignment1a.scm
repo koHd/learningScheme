e48f36c8569677ff4d4825fd8845de225fd3132b
Shane Farrell <shane.farrell@yahoo.ie>
2015-11-19 17:00:37 +0000

    set-equal? function added.
    No input checking as per assignment specification. Will only work if the user keeps the contract.

--- assignment1a.scm
+++ assignment1a.scm
@@ -32,18 +32,23 @@
         [else (set-union (cdr setA) (add-element (car setA) setUnion))]))
 
 (define (set-intersection set1 set2)
-        (define setA (list-to-set set1))
+    (define setA (list-to-set set1))
         (define setB (list-to-set set2))
         (cond
             [(empty? setA) '()]
             [(member? (car setA) setB)
                 (cons (car setA) (set-intersection (cdr setA) setB))]
             [else (set-intersection (cdr setA) setB)]))
-
+ 
 (define (set-difference set1 set2)
     (cond
         [(empty? set1) '()]
         [(not (member? (car set1) set2))
             (cons (car set1) (set-difference (cdr set1) set2))]
         [else (set-difference (cdr set1) set2)]))
-            
+
+(define (set-equal? set1 set2)
+    (cond
+        [(empty? set1) #t]
+        [(not (member? (car set1) set2)) #f]
+        [else (set-equal? (cdr set1) set2)])) 
